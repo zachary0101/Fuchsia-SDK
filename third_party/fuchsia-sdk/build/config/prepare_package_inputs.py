c3aa8ee7ab8e11537a01b16a0e32a9bc9f1e082e
Clayton Wilkinson <wilkinsonclay@google.com>
2020-01-14 10:12:31 -0800

    [gn-sdk] Updated build templates based on review
    These changes are from feedback during review.
    
    Change-Id: I649e66aac3d674234aaf535d48511accfb7508af

--- third_party/fuchsia-sdk/build/config/prepare_package_inputs.py
+++ third_party/fuchsia-sdk/build/config/prepare_package_inputs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 # Copyright 2019 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -17,25 +18,17 @@ import sys
 import tempfile
 
 
-def MakePackagePath(file_path, roots):
+def make_package_path(file_path, roots):
     """Computes a path for |file_path| relative to one of the |roots|.
 
   Args:
-
     file_path: The file path to relativize.
-    roots: A list of directory paths which may serve as a relative root
-         for |file_path|.
-
-  Examples:
-
-  >>> MakePackagePath('/foo/bar.txt', ['/foo/'])
-  'bar.txt'
-
-  >>> MakePackagePath('/foo/dir/bar.txt', ['/foo/'])
-  'dir/bar.txt'
-
-  >>> MakePackagePath('/foo/out/Debug/bar.exe', ['/foo/', '/foo/out/Debug/'])
-  'bar.exe'
+    roots: A list of directory paths which may serve as a relative root for
+      |file_path|.
+  Examples:  >>> make_package_path('/foo/bar.txt', ['/foo/']) 'bar.txt'  >>>
+    make_package_path('/foo/dir/bar.txt', ['/foo/']) 'dir/bar.txt'  >>>
+    make_package_path('/foo/out/Debug/bar.exe', ['/foo/', '/foo/out/Debug/'])
+    'bar.exe'
   """
 
     # Prevents greedily matching against a shallow path when a deeper, better
@@ -48,26 +41,28 @@ def MakePackagePath(file_path, roots):
 
         if file_path.startswith(next_root):
             relative_path = file_path[len(next_root):]
-
             return relative_path
 
     return file_path
 
 
-def _GetStrippedPath(bin_path):
+def _get_stripped_path(bin_path):
     """Finds the stripped version of |bin_path| in the build output directory.
+
+        returns |bin_path| if no stripped path is found.
   """
     stripped_path = bin_path.replace('lib.unstripped/',
                                      'lib/').replace('exe.unstripped/', '')
     if os.path.exists(stripped_path):
         return stripped_path
     else:
-        shutil.copyfile(bin_path, stripped_path)
-        return stripped_path
+        return bin_path
 
 
-def _IsBinary(path):
-    """Checks if the file at |path| is an ELF executable by inspecting its FourCC header.
+def _is_binary(path):
+    """Checks if the file at |path| is an ELF executable.
+
+        This is done by inspecting its FourCC header.
   """
 
     with open(path, 'rb') as f:
@@ -75,8 +70,9 @@ def _IsBinary(path):
     return file_tag == '\x7fELF'
 
 
-def _WriteBuildIdsTxt(binary_paths, ids_txt_path):
+def _write_build_ids_txt(binary_paths, ids_txt_path):
     """Writes an index text file that maps build IDs to the paths of unstripped
+
   binaries.
   """
 
@@ -90,7 +86,7 @@ def _WriteBuildIdsTxt(binary_paths, ids_txt_path):
     with open(ids_txt_path, 'w') as ids_file:
         # Create a set to dedupe stripped binary paths in case both the stripped and
         # unstripped versions of a binary are specified.
-        stripped_binary_paths = set(map(_GetStrippedPath, binary_paths))
+        stripped_binary_paths = set(map(_get_stripped_path, binary_paths))
         readelf_stdout = subprocess.check_output(
             ['readelf', '-n'] + list(stripped_binary_paths))
 
@@ -124,35 +120,35 @@ def _WriteBuildIdsTxt(binary_paths, ids_txt_path):
     assert not unprocessed_binary_paths
 
 
-def ParseComponent(component_info_file):
+def _parse_component(component_info_file):
     component_info = json.load(open(component_info_file, 'r'))
     return component_info
 
 
-def GetAppFilename(component_info):
+def _get_app_filename(component_info):
     for c in component_info:
         if c.get('type') == 'dep':
             pos = c.get('source').find(':')
             return c.get('source')[pos + 1:]
 
 
-def GetManifestFile(component_info):
+def _get_manifest_file(component_info):
     for c in component_info:
         if c.get('type') == 'manifest':
             return str(c.get('source'))
 
 
-def BuildManifest(args):
-    component_info_list = ParseComponent(args.json_file)
+def _build_manifest(args):
+    component_info_list = _parse_component(args.json_file)
     binaries = []
     with open(args.manifest_path, 'w') as manifest, \
          open(args.depfile_path, 'w') as depfile:
         for component_info in component_info_list:
-            app_filename = GetAppFilename(component_info)
+            app_filename = _get_app_filename(component_info)
 
             # Process the runtime deps file for file paths, recursively walking
             # directories as needed.
-            # MakePackagePath() may relativize to either the source root or output
+            # make_package_path() may relativize to either the source root or output
             # directory.
             # runtime_deps may contain duplicate paths, so use a set for
             # de-duplication.
@@ -173,11 +169,11 @@ def BuildManifest(args):
             app_found = False
             excluded_files_set = set(args.exclude_file)
             for current_file in expanded_files:
-                if _IsBinary(current_file):
+                if _is_binary(current_file):
                     binaries.append(current_file)
-                current_file = _GetStrippedPath(current_file)
+                current_file = _get_stripped_path(current_file)
 
-                in_package_path = MakePackagePath(
+                in_package_path = make_package_path(
                     current_file, [gen_dir, args.root_dir, args.out_dir])
                 if in_package_path == app_filename:
                     app_found = True
@@ -204,7 +200,7 @@ def BuildManifest(args):
                 json.dump({'version': '0', 'name': args.app_name}, package_json)
 
             # Write component manifest file.
-            cmx_source = GetManifestFile(component_info)
+            cmx_source = _get_manifest_file(component_info)
             cmx_file_path = os.path.join(
                 os.path.dirname(args.manifest_path), args.app_name + '.cmx')
             manifest.write(
@@ -236,7 +232,7 @@ def BuildManifest(args):
                             for f in expanded_files
                         ])))
 
-            _WriteBuildIdsTxt(binaries, args.build_ids_file)
+            _write_build_ids_txt(binaries, args.build_ids_file)
 
     return 0
 
@@ -267,7 +263,7 @@ def main():
 
     args = parser.parse_args()
 
-    return BuildManifest(args)
+    return _build_manifest(args)
 
 
 if __name__ == '__main__':
