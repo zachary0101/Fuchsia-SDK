ff668fdfeb41083c39faed33443e4ff01156d845
Clayton Wilkinson <wilkinsonclay@google.com>
2020-02-27 23:15:04 +0000

    [googletest] Refactoring loop_fixture structure.
    This refactors loop_fixture so the header files are includable
    via config and not include the source files.
    
    Change-Id: I6fe19dd6e73047da06daf61540dcf8f51b00c446

--- third_party/googletest/loop_fixture/real_loop_fixture.cc
+++ third_party/googletest/loop_fixture/real_loop_fixture.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "real_loop_fixture.h"
+#include "lib/gtest/real_loop_fixture.h"
 
 #include <lib/async/cpp/task.h>
 #include <lib/zx/clock.h>
