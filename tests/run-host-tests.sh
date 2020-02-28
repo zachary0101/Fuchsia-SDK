9cc52f4fc496c033f2cfe2e817cec40e5529f040
Wayne Piekarski <waynepie@google.com>
2020-02-27 17:19:43 -0800

    [samples] Fix host tests so they run relative to $REPO_ROOT and not $PWD
    Change-Id: I475059d9fe838311cf5fca163104f353e4935f63

--- tests/run-host-tests.sh
+++ tests/run-host-tests.sh
@@ -9,21 +9,25 @@ err_print() {
 }
 trap 'err_print $LINENO' ERR
 DEBUG_LINE() {
-    $@
+    "$@"
 }
 
 TEST_SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Common functions.
+# shellcheck disable=SC1090
 source "${TEST_SRC_DIR}/../scripts/common.sh" || exit $?
 REPO_ROOT=$(get_gn_root) # finds path to REPO_ROOT
 
 echo
 echo "==== Run host tests ===="
-for dir in `ls out`; do
-  for i in `cat out/$dir/all_host_tests.txt`; do
-    out/$dir/$i
-  done
+for dir in "${REPO_ROOT}/out"/*; do
+  [[ -e "$dir" ]] || break # Handle case with empty out directory
+
+  while IFS= read -r testname
+  do
+    "$dir/$testname"
+  done < "$dir/all_host_tests.txt"
 done
 
 echo
